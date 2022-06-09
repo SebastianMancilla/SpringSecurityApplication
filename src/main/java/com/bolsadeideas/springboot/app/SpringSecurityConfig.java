5ef03eac08ff72fa5e827dea7d4f08395fe6c2e5
SebastianMancilla <gonzalezsebas43@gmail.com>
2022-06-09 10:13:52 -0600

    updates, implementation authentication JDBC in Login with SQL

--- src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -1,16 +1,14 @@
 package com.bolsadeideas.springboot.app;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.bolsadeideas.springboot.app.auth.handler.LoginSuccessHandler;
 
@@ -20,6 +18,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Autowired
     private LoginSuccessHandler successHandler;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
@@ -41,24 +45,24 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
         .exceptionHandling().accessDeniedPage("/error_403");
     }
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
 
     @Autowired
-    public void configurerGlobal(AuthenticationManagerBuilder builder){
+    public void configurerGlobal(AuthenticationManagerBuilder builder)throws Exception{
 
-        PasswordEncoder encoder = passwordEncoder();
-        UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+        builder.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder)
+        .usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username=?")
+        .authoritiesByUsernameQuery("SELECT u.username, a.authority FROM authorities a INNER JOIN users u ON(a.user_id = u.id) WHERE u.username=?");
 
-        try {
-            builder.inMemoryAuthentication()
-            .withUser(users.username("admin").password("12345").roles("ADMIN", "user"))
-            .withUser(users.username("sebas").password("1234").roles("USER"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // PasswordEncoder encoder = passwordEncoder;
+        // UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+
+        // try {
+        //     builder.inMemoryAuthentication()
+        //     .withUser(users.username("admin").password("12345").roles("ADMIN", "user"))
+        //     .withUser(users.username("sebas").password("1234").roles("USER"));
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
     }
     
 }
